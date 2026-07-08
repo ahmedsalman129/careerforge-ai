bf6905ffd92beb67f72d739fa1a2229a80d817a6
Ahmed Salman <ahmedsalman12900@gmail.com>
2026-07-08 11:51:04 +0500

    Connect frontend to Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api",
+  baseURL: "https://careerforge-ai-2-clz9.onrender.com/api",
 });
 
 export default api;
